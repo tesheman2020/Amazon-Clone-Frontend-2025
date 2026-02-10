40f8d5ef249ec0f4c33104b4548756326c8120e3
tesheman2020 <tesheman2020@gmail.com>
2026-02-10 18:33:30 -0500

    Updated ProtectedRoute.jsx

--- amazon-clone/src/Components/ProtectedRoute/ProtectedRoute.jsx
+++ amazon-clone/src/Components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,9 +1,8 @@
 import React, { useContext } from "react";
-import { useNavigate, Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { DataContext } from "../DataProvider/DataProvider";
 
 const ProtectedRoute = ({ children, msg, redirect }) => {
-  const navigate = useNavigate();
   const { user } = useContext(DataContext);
 
   // 1️⃣ Firebase still checking auth
@@ -21,3 +20,27 @@ const ProtectedRoute = ({ children, msg, redirect }) => {
 };
 
 export default ProtectedRoute;
+
+// import React, { useContext } from "react";
+// import { useNavigate, Navigate } from "react-router-dom";
+// import { DataContext } from "../DataProvider/DataProvider";
+
+// const ProtectedRoute = ({ children, msg, redirect }) => {
+//   const Navigate = useNavigate();
+//   const { user } = useContext(DataContext);
+
+//   // 1️⃣ Firebase still checking auth
+//   if (user === undefined) {
+//     return <h2 style={{ padding: "2rem" }}>Checking authentication...</h2>;
+//   }
+
+//   // 2️⃣ Not logged in
+//   if (user === null) {
+//     return <Navigate to="/auth" replace state={{ msg, redirect }} />;
+//   }
+
+//   // 3️⃣ Logged in
+//   return children;
+// };
+
+// export default ProtectedRoute;
