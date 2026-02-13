1d3a406c4675a3d26f7e21ee3655ab29706fe168
tesheman2020 <tesheman2020@gmail.com>
2026-02-13 00:25:19 -0500

    Fix App.js

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -8,9 +8,7 @@
 // export default App;
 
 
-
-
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import Routing from "./Router.jsx";
 import { auth } from "./Components/Utility/firebase";
 import { DataContext } from "./Components/DataProvider/DataProvider";
@@ -18,9 +16,9 @@ import { Type } from "./Components/Utility/action.Type";
 
 function App() {
   const { dispatch } = useContext(DataContext);
+  const [loading, setLoading] = useState(true); // 🔹 Track if auth is ready
 
   useEffect(() => {
-    // This observer restores the user session from local storage automatically
     const unsubscribe = auth.onAuthStateChanged((authUser) => {
       if (authUser) {
         dispatch({
@@ -33,16 +31,54 @@ function App() {
           user: null,
         });
       }
+      setLoading(false); // 🔹 Auth check done
     });
 
-    // Cleanup subscription on unmount
     return () => unsubscribe();
   }, [dispatch]);
+
+  // 🔹 Wait until auth is initialized
+  if (loading) return null; // or a spinner <div>Loading...</div>
 
   return <Routing />;
 }
 
 export default App;
+
+
+// import React, { useEffect, useContext } from "react";
+// import Routing from "./Router.jsx";
+// import { auth } from "./Components/Utility/firebase";
+// import { DataContext } from "./Components/DataProvider/DataProvider";
+// import { Type } from "./Components/Utility/action.Type";
+
+// function App() {
+//   const { dispatch } = useContext(DataContext);
+
+//   useEffect(() => {
+//     // This observer restores the user session from local storage automatically
+//     const unsubscribe = auth.onAuthStateChanged((authUser) => {
+//       if (authUser) {
+//         dispatch({
+//           type: Type.SET_USER,
+//           user: authUser,
+//         });
+//       } else {
+//         dispatch({
+//           type: Type.SET_USER,
+//           user: null,
+//         });
+//       }
+//     });
+
+//     // Cleanup subscription on unmount
+//     return () => unsubscribe();
+//   }, [dispatch]);
+
+//   return <Routing />;
+// }
+
+// export default App;
 
 // import React, { useEffect, useContext } from "react";
 // import Routing from "./Router.jsx";
